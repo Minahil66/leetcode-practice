ff13018ef2d9da8f180d488408e6c3f808fd08e1
Minahil66 <minahil.066@outlook.com>
2025-11-20 20:52:54 +0500

    2025-11-20T20:52:54+05:00

---
+++ climbing stairs.c
@@ -0,0 +1,14 @@
+// Last updated: 2025-11-20T20:52:54+05:00
+// Last updated: 2025-11-20T20:52:45+05:00
+// Last updated: 2025-11-20T20:34:13+05:00
+// int climbStairs(int n) {
+//     if (n <= 2) return n;
+    
+//     int first = 1, second = 2;
+//     for (int i = 3; i <= n; i++) {
+//         int current = first + second;
+//         first = second;
+//         second = current;
+//     }
+//     return second;
+// }
